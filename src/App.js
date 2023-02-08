f5c76a16f20127a84e7b33a8a1c7618087e9ab92
Radwa <Radwa2484@gmail.com>
2023-02-09 00:04:57 +0200

    yes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter , Routes , Route} from "react-router-dom" ;
+import Home from "./components/home"
+import About from "./components/about"
+import Contacts from "./components/contacts"
+
 
 function App() {
-  return (
-    
-  );
+  return (<BrowserRouter>
+  <Routes>
+    <Route path='/' element={<h1> welcome to the main page </h1>} />
+    <Route path="/home" element={<Home />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="/contacts" element={<Contacts />} />
+    </Routes>
+    </BrowserRouter>
+   
+  )
 }
 
-export default App;
+export default App
